b188dda2dabe1212716801dd0a61e8fe294100d6
Paul Walker <pjwalker76@gmail.com>
2016-03-14 12:34:48 -0400

    Test load

--- tests/gdocs_spec.js
+++ tests/gdocs_spec.js
@@ -1,7 +1,16 @@
+import Gdocs from '../src/gdocs';
+
 describe('1+1', () => {
   let sum = 1+1;
 
   it('should be 2', () => {
     expect(2).toBe(2);
+  });
+});
+
+describe('Gdocs module', () => {
+  it('Should load', () => {
+    expect(typeof Gdocs).toBe('object');
+    expect(Gdocs.__type__).toBe('gdocs');
   });
 });
